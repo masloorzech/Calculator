6b75b267b707209ff0a1b6f7f0d65c9d03cb2e98
AntoniIwan <245819@edu.p.lodz.pl>
2025-03-12 17:03:00 +0100

    Addad 3 layouts

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.util.Log
 import kotlin.math.E
 import kotlin.math.PI
 import kotlin.math.acos
